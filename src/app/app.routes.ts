b225f7473db853b7c6ead34e42472d6811c9bcd0
Wadii Douihech <wadiidouihech@gmail.com>
2025-04-27 00:48:27 +0100

    testing components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./login/login.component').then(
+                (m) => m.LoginComponent
+            );
+        },
+    }
+];
